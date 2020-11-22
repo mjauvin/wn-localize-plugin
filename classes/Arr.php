2ff4cc9d91afa8c174ffc41fa26c662b9e0052e0
Lieszkovszky L. L <16566226+lieszkol@users.noreply.github.com>
2020-11-22 21:51:02 +0100

    Update Arr.php

--- classes/Arr.php
+++ classes/Arr.php
@@ -3,16 +3,16 @@
 use Illuminate\Support\Arr as ArrHelper;
 		
 if (!function_exists('array_undot')) {
-	/**
-	 * Transform a dot-notated array into a normal array.
-	 *
-	 * @param array $dotArray
-	 * @return array
-	 */
-	function array_undot(array $dotArray)
-	{
-		return \StudioAzura\Localize\Classes\Arr::undot($dotArray);
-	}
+    /**
+     * Transform a dot-notated array into a normal array.
+     *
+     * @param array $dotArray
+     * @return array
+     */
+    function array_undot(array $dotArray)
+    {
+        return Arr::undot($dotArray);
+    }
 }
 
 class Arr extends ArrHelper
