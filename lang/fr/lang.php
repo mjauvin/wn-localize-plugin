2991f056cd8635b67396ec329675095defa2e483
Marc Jauvin <marc.jauvin@gmail.com>
2020-12-01 15:12:43 -0500

    add french localization strings

---
+++ lang/fr/lang.php
@@ -0,0 +1,30 @@
+<?php return [
+    'plugin' => [
+        'description' => 'Permet la gestion des chaines de localisation par les administrateurs en format YAML',
+    ],
+    'localization' => [
+        'add_missing_strings' => 'Ajouter les chaines manquantes',
+        'confirm_reset' => 'Vous confirmez la réinitialisation des traductions?',
+        'files' => 'Fichier de langue',
+        'language' => 'Langage',
+        'reset' => 'Réinitialiser les traductions',
+        'select_file' => '-- selectionner le fichier de langue --',
+    ],
+    'languagelist' => [
+        'add' => 'Ajouter',
+        'search' => 'Chercher',
+    ],
+    'permissions' => [
+        'manage_localizations' => [
+            'tab' => 'Plugin de localisation',
+            'label' => 'Manage Plugins Localization Files',
+        ],
+        'manage_settings' => [
+            'label' => 'Gérer les paramètres de localisation',
+        ],
+    ],
+    'settings' => [
+        'description' => 'Gérer les paramètres de localisation',
+        'hide_builder' => 'Cacher le menu du plugin RainLab.Builder',
+    ],
+];
