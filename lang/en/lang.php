574d4dd922610b036d07f3dbc6c597c4388b9d82
Marc Jauvin <marc.jauvin@gmail.com>
2020-12-01 15:13:12 -0500

    more localization strings now available

--- lang/en/lang.php
+++ lang/en/lang.php
@@ -6,10 +6,16 @@ return [
         'description' => 'Allow admins to override Plugin localization files from the backend UI',
     ],
     'localization' => [
-        'select_file' => '-- select language file --',
-        'files' => 'Language Files',
-        'reset' => 'Reset Overrides',
+        'add_missing_strings' => 'Add missing strings',
         'confirm_reset' => 'Reset the override language file?',
+        'files' => 'Language Files',
+        'language' => 'Language',
+        'reset' => 'Reset Overrides',
+        'select_file' => '-- select language file --',
+    ],
+    'languagelist' => [
+        'add' => 'Add',
+        'search' => 'Search',
     ],
     'permissions' => [
         'manage_localizations' => [
