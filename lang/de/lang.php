c7c835ebfcdd78e208ad6a93f23dbd67c0883336
Sven Suchan <f.duke@web.de>
2020-12-01 14:18:29 +0100

    Create lang.php
    - german language support added

---
+++ lang/de/lang.php
@@ -0,0 +1,28 @@
+<?php
+
+return [
+    'plugin' => [
+        'name' => 'Lokalisierung',
+        'description' => 'Erlaubt Admins die Lokalisierungsdateien zu überschreiben direkt aus dem Backend des October CMS.',
+    ],
+    'localization' => [
+        'select_file' => '-- wähle die Sprachdatei aus --',
+        'files' => 'Sprachdateien',
+        'reset' => 'Überschreibungen reseten',
+        'confirm_reset' => 'Die Überschreibung-Sprachdatei zurücksetzen?',
+    ],
+    'permissions' => [
+        'manage_localizations' => [
+            'tab' => 'Lokaliserung Plugin',
+            'label' => 'Verwalte die Plugins Lokalisierungs Dateien',
+        ],
+        'manage_settings' => [
+            'label' => 'Manage Localization Settings',
+        ],
+    ],
+    'settings' => [
+        'label' => 'Lokalisierung',
+        'description' => 'Verwalte die Lokalisierungseinstellungen',
+        'hide_builder' => 'Verstecke den RainLab.Builder Menüeintrag',
+    ],
+];
